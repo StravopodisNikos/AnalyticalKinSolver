d4204f71ecd5e32b8cce4308f812bffffc46033b
SyrosRoboticsResearch1 <stravnik.rob@gmail.com>
2023-07-27 16:56:12 +0300

    added fwd kin fn, connected the 2 classes

--- AnalyticalKinSolver.h
+++ AnalyticalKinSolver.h
@@ -13,20 +13,34 @@ namespace kinematics_ns {
     class AnalyticalKinSolver: public screws_utils_ns::screws4robotics
     {
     private:
-
-    public:
-        AnalyticalKinSolver(/* args */);
-        ~AnalyticalKinSolver();
-    };
-
-    class AnalyticalJacobians: public screws_utils_ns::screws4robotics
-    {
-    private:
-        //Structure2Pseudos _robot_def2;
-        //Structure3Pseudos _robot_def3;
-        //Structure4Pseudos _robot_def4;
         std::shared_ptr<RobotParametersBase> _robot_def;
         uint8_t _dof;
+        Eigen::Vector3f _p_tcp_sp; // spatial position vector of the TCP
+        // Screw utils 
+        Eigen::Isometry3f _Pi[DOF-1];
+        Eigen::Isometry3f _exp; // saves current 4x4 twist exponential matrix
+        Eigen::Matrix<float, 6, 6> _ad;  // saves current 6x6 adjoint matrix 
+               
+    public:
+        AnalyticalKinSolver(/* args */);
+        AnalyticalKinSolver(const std::shared_ptr<RobotParametersBase>& robot_def);
+        ~AnalyticalKinSolver();
+        
+        void PseudoTfs();
+        Eigen::Isometry3f ForwardKinematicsTCP(float *q);
+
+        void PrintTransform(Stream& serialPort, Eigen::Isometry3f g);
+    };
+
+    //class AnalyticalJacobians: public screws_utils_ns::screws4robotics
+    class AnalyticalJacobians: public AnalyticalKinSolver
+    {
+    private:
+        std::shared_ptr<RobotParametersBase> _robot_def;
+        uint8_t _dof;
+        float *_q_cur;
+        Eigen::Isometry3f _g_tcp_sp; // current spatial tcp transform
+        Eigen::Vector3f _p_tcp_sp; // spatial position vector of the TCP
 
         // Screw utils 
         Eigen::Isometry3f _Pi[DOF-1];
@@ -34,18 +48,18 @@ namespace kinematics_ns {
         Eigen::Matrix<float, 6, 6> _ad;  // saves current 6x6 adjoint matrix
         Eigen::Matrix<float, 6, DOF> _Jsp;
         Eigen::Matrix<float, 6, DOF> _Jbd;
-        Eigen::Matrix<float, DOF, DOF> _Jt;
+        Eigen::Matrix<float, DOF, DOF> _Jtcp;
 
+        void _update_q_rad(float *q); // auxiliary for test only
+        void _update_p_tcp_sp();
     public:
         AnalyticalJacobians();
         AnalyticalJacobians(const std::shared_ptr<RobotParametersBase>& robot_def);
-
         ~AnalyticalJacobians();
-
-        void PseudoTfs();
 
         void SpatialJacobian(float *q); // eq.3.54, A Mathematical Introduction to robotic Manipulation Book
         void BodyJacobian(float *q);
+        void ToolJacobian(float *q, char jacobian_char_id); // char sets the matrix calculation preference
 
         void PrintJacobian(Stream& serialPort, char jacobian_char_id);
     };
